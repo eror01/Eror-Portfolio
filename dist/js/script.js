1b48eaf251e8839d8dc163ded1a965f8530f557a
Eror Aleksandar <eror.aleksandar01@gmail.com>
2021-03-11 23:27:07 +0100

    Added media queries and finished

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,15 @@
-const certBtn = document.querySelector('.btn-cert');
 const modalContainer = document.querySelector('.modal-container');
+const certBtn = document.querySelector('.btn-cert');
 const closeBtn = document.querySelector('.close-modal');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const header = document.querySelector('.header');
+const closeHeader = document.querySelector('.btn__close-header');
+const nextBtn = document.querySelector('.btn-next');
+const prevBtn = document.querySelector('.btn-prev');
+const projectsShow = document.querySelector('.projects__showcase');
+
+let projectsContainerWidth = document.querySelector('.projects__container-sub').offsetWidth;
+let index = 0;
 
 // Functions
 const highlightActive = () =>{
@@ -31,27 +40,67 @@ const highlightActive = () =>{
 
 };
 
+
 // Event Listeners
 window.addEventListener('scroll', highlightActive);
 window.addEventListener('click', highlightActive);
+
+// Opening modal
 certBtn.addEventListener('click', () => { 
     modalContainer.classList.add('show-modal');
     document.querySelector('body').style.overflow = 'hidden';
 });
+
+// Closing modal
 closeBtn.addEventListener('click', () => {
     modalContainer.classList.remove('show-modal');
     document.querySelector('body').style.overflow = 'visible';
 });
-window.addEventListener('click', e =>{
-    if(e.key === 'Escape'){
+// Clicking on modal__container closes
+modalContainer.addEventListener('click', e =>{
         modalContainer.classList.remove('show-modal');
         document.querySelector('body').style.overflow = 'visible';
-    }
-    
 });
+// Pressing escape key closes modal__container
 window.addEventListener('keydown', e => {
     if(e.key === 'Escape'){
         modalContainer.classList.remove('show-modal');
         document.querySelector('body').style.overflow = 'visible';
     }
+});
+window.addEventListener('resize', () =>{
+    projectsContainerWidth = document.querySelector('.projects__container-sub').offsetWidth;
+});
+// Hamburger menu
+hamburgerMenu.addEventListener('click', () => {
+    header.classList.add('change');
+    hamburgerMenu.style.zIndex = '0';
+});
+closeHeader.addEventListener('click', () =>{
+    header.classList.remove('change');
+    hamburgerMenu.style.zIndex = '1000';
+});
+
+nextBtn.addEventListener('click', () =>{
+    index++;
+    prevBtn.classList.add('show');
+    projectsShow.style.transform = `
+        translate(-${index * projectsContainerWidth}px)
+    `;
+
+    if(projectsShow.offsetWidth - 
+        (index * projectsContainerWidth) < projectsContainerWidth){
+            nextBtn.classList.add('hide');
+        }
+});
+prevBtn.addEventListener('click', () =>{
+    index--;
+    nextBtn.classList.remove('hide');
+    if(index === 0){
+        prevBtn.classList.remove('show');
+    }
+
+    projectsShow.style.transform = `
+        translate(-${index * projectsContainerWidth}px)
+    `;
 });
